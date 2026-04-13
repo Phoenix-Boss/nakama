50d78287e95dfca04a7c35b5184a2a09c91e08c2
Phoenix-Boss <bossblingzs@gmail.com>
2026-04-13 08:49:08 +0100

    Fix: remove BOM, use full nakama binary path

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
-﻿#!/bin/sh
+#!/bin/sh
 echo "=== Starting Nakama with Supabase ==="
 grep "address" /nakama/data/nakama.yml
 /keepalive.sh &
-exec nakama --config /nakama/data/nakama.yml --database.address "postgres:l6TuVaUiqmhWbbVO@db.atojskxrxfsbpeefigtm.supabase.co:5432/postgres?sslmode=require"
+exec /nakama/nakama --config /nakama/data/nakama.yml --database.address "postgres:l6TuVaUiqmhWbbVO@db.atojskxrxfsbpeefigtm.supabase.co:5432/postgres?sslmode=require"
