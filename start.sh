ad2ff2d6eba0b221b1d948ea565d06a5c7944021
Phoenix-Boss <bossblingzs@gmail.com>
2026-04-13 09:09:46 +0100

    Fix: run migrate up before starting Nakama

--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 echo '=== Starting Nakama with Supabase ==='
-grep 'address' /nakama/data/nakama.yml
 /keepalive.sh &
+echo 'Running migrations...'
+/nakama/nakama migrate up --config /nakama/data/nakama.yml --database.address 'postgres.atojskxrxfsbpeefigtm:l6TuVaUiqmhWbbVO@aws-0-eu-west-1.pooler.supabase.com:6543/postgres?sslmode=require'
+echo 'Starting Nakama server...'
 exec /nakama/nakama --config /nakama/data/nakama.yml --database.address 'postgres.atojskxrxfsbpeefigtm:l6TuVaUiqmhWbbVO@aws-0-eu-west-1.pooler.supabase.com:6543/postgres?sslmode=require'
