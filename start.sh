0837bd7e0095769768df886b932757db062dbc44
Phoenix-Boss <bossblingzs@gmail.com>
2026-04-13 09:13:57 +0100

    Fix: use session pooler port 5432 for prepared statement support

--- start.sh
+++ start.sh
@@ -2,6 +2,6 @@
 echo '=== Starting Nakama with Supabase ==='
 /keepalive.sh &
 echo 'Running migrations...'
-/nakama/nakama migrate up --config /nakama/data/nakama.yml --database.address 'postgres.atojskxrxfsbpeefigtm:l6TuVaUiqmhWbbVO@aws-0-eu-west-1.pooler.supabase.com:6543/postgres?sslmode=require'
+/nakama/nakama migrate up --config /nakama/data/nakama.yml --database.address 'postgres.atojskxrxfsbpeefigtm:l6TuVaUiqmhWbbVO@aws-0-eu-west-1.pooler.supabase.com:5432/postgres?sslmode=require'
 echo 'Starting Nakama server...'
-exec /nakama/nakama --config /nakama/data/nakama.yml --database.address 'postgres.atojskxrxfsbpeefigtm:l6TuVaUiqmhWbbVO@aws-0-eu-west-1.pooler.supabase.com:6543/postgres?sslmode=require'
+exec /nakama/nakama --config /nakama/data/nakama.yml --database.address 'postgres.atojskxrxfsbpeefigtm:l6TuVaUiqmhWbbVO@aws-0-eu-west-1.pooler.supabase.com:5432/postgres?sslmode=require'
