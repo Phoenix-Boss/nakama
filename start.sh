b583ddd3a7eb7144dc89d4bffe30623c59f231fe
Phoenix <126855052+Phoenix-Boss@users.noreply.github.com>
2026-04-21 14:02:49 +0100

    render

--- start.sh
+++ start.sh
@@ -1,7 +1,20 @@
 #!/bin/sh
+
 echo '=== Starting Nakama with Supabase ==='
+
+# Create necessary directories
+mkdir -p /nakama/data/logs
+mkdir -p /nakama/data/modules
+
+# Start keepalive in background
 /keepalive.sh &
+
 echo 'Running migrations...'
-/nakama/nakama migrate up --config /nakama/data/nakama.yml --database.address 'postgres.atojskxrxfsbpeefigtm:l6TuVaUiqmhWbbVO@aws-0-eu-west-1.pooler.supabase.com:5432/postgres?sslmode=require'
+/nakama/nakama migrate up \
+  --config /nakama/data/nakama.yml \
+  --database.address 'postgres.atojskxrxfsbpeefigtm:l6TuVaUiqmhWbbVO@aws-0-eu-west-1.pooler.supabase.com:5432/postgres?sslmode=require'
+
 echo 'Starting Nakama server...'
-exec /nakama/nakama --config /nakama/data/nakama.yml --database.address 'postgres.atojskxrxfsbpeefigtm:l6TuVaUiqmhWbbVO@aws-0-eu-west-1.pooler.supabase.com:5432/postgres?sslmode=require'
+exec /nakama/nakama \
+  --config /nakama/data/nakama.yml \
+  --database.address 'postgres.atojskxrxfsbpeefigtm:l6TuVaUiqmhWbbVO@aws-0-eu-west-1.pooler.supabase.com:5432/postgres?sslmode=require'
